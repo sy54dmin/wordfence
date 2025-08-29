31b8cbcf3835a50059d5762eb10ca845dc9f5c31
mohamedhk2 <mohamedhk2@yahoo.fr>
2025-08-29 01:07:04 +0100

    update license activation

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-<p align="center"><img src="https://github.com/wp-activators/.github/blob/main/FreePalestine.png" height="250"></p>
+<p align="center"><img src="https://raw.githubusercontent.com/wp-activators/.github/refs/heads/main/images/FreePalestine.png" height="250"></p>
 
+---
 
+## ⚠️ Disclaimer ⚠️
+
+**For testing purposes only, not for commercial use!**  
+Please note that this repository is intended solely for experimentation, learning, and **security testing**.
+
+---
 # Wordfence Activator
 
 Wordfence Security Plugin Activator
--- main.php
+++ main.php
@@ -3,8 +3,8 @@
  * @wordpress-plugin
  * Plugin Name:       Wordfence Security Activ@tor
  * Plugin URI:        https://bit.ly/wf-act
- * Description:       Wordfence Security Plugin Activ@tor ✨ (Let's Play a Game)
- * Version:           1.4.0
+ * Description:       Wordfence Security Plugin Activ@tor 🔥 (The Game has Just Begun...)
+ * Version:           1.5.0
  * Requires at least: 5.9.0
  * Requires PHP:      7.2
  * Author:            moh@medhk2
@@ -17,46 +17,50 @@ $PLUGIN_DOMAIN = 'wordfence-activ@tor';
 $RemainingDays = 365 * 10;
 extract( require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php' );
 if (
-	$admin_notice_ignored()
-	|| $admin_notice_plugin_install( 'wordfence/wordfence.php', 'wordfence', 'Wordfence Security', $PLUGIN_NAME, $PLUGIN_DOMAIN )
-	|| $admin_notice_plugin_activate( 'wordfence/wordfence.php', $PLUGIN_NAME, $PLUGIN_DOMAIN )
+        $admin_notice_ignored()
+        || $admin_notice_plugin_install( 'wordfence/wordfence.php', 'wordfence', 'Wordfence Security', $PLUGIN_NAME, $PLUGIN_DOMAIN )
+        || $admin_notice_plugin_activate( 'wordfence/wordfence.php', $PLUGIN_NAME, $PLUGIN_DOMAIN )
 ) {
-	return;
+    return;
 }
 
 $init = function () use ( $RemainingDays, $PLUGIN_NAME ) {
-	try {
-		wfOnboardingController::_markAttempt1Shown();
-		wfConfig::set( 'onboardingAttempt3', wfOnboardingController::ONBOARDING_LICENSE );
-		if ( empty( wfConfig::get( 'apiKey' ) ) ) {
-			wordfence::ajax_downgradeLicense_callback();
-		}
+    try {
+        wfOnboardingController::_markAttempt1Shown();
+        wfConfig::set( 'onboardingAttempt3', wfOnboardingController::ONBOARDING_LICENSE );
+        if ( empty( wfConfig::get( 'apiKey' ) ) ) {
+            wordfence::ajax_downgradeLicense_callback();
+        }
         wfConfig::set( 'is' . 'Paid', true );
-		wfConfig::set( 'keyType', wfLicense::KEY_TYPE_PAID_CURRENT );
-		wfConfig::set( 'premiumNextRenew', time() + $RemainingDays * 86400 );
-		wfWAF::getInstance()->getStorageEngine()->setConfig( 'wafStatus', wfFirewall::FIREWALL_MODE_ENABLED );
-	} catch ( Exception $exception ) {
-		add_action( 'admin_notices', function () use ( $exception, $PLUGIN_NAME ) { ?>
+        wfConfig::set( 'success', 1 );
+        wfConfig::set( 'keyType', wfLicense::KEY_TYPE_PAID_CURRENT );
+        wfConfig::set( 'premiumNextRenew', time() + $RemainingDays * 86400 );
+        wfConfig::set( 'licenseType', wfLicense::TYPE_RESPONSE );
+        wfWAF::getInstance()->getStorageEngine()->setConfig( 'wafStatus', wfFirewall::FIREWALL_MODE_ENABLED );
+    } catch ( Exception $exception ) {
+        add_action( 'admin_notices', function () use ( $exception, $PLUGIN_NAME ) { ?>
             <div class="notice notice-error">
             <p><?php
-				printf(
-				/* translators: %s: plugin name */
-					esc_html__( $PLUGIN_NAME . ' error: %s', 'wordfence-activ@tor' ),
-					esc_html( $exception->getMessage() )
-				); ?></p>
+                printf(
+                /* translators: %s: plugin name */
+                        esc_html__( $PLUGIN_NAME . ' error: %s', 'wordfence-activ@tor' ),
+                        esc_html( $exception->getMessage() )
+                ); ?></p>
             </div><?php
-		} );
-	}
+        } );
+    }
 };
 
 add_action( 'plugins_loaded', function () use ( $RemainingDays, $init ) {
-	if ( class_exists( 'wfLicense' ) ) {
-		$init();
-		wfLicense::current()->setType( wfLicense::TYPE_RESPONSE );
-		wfLicense::current()->setPaid( true );
-		wfLicense::current()->setRemainingDays( $RemainingDays );
-		wfLicense::current()->setConflicting( false );
-		wfLicense::current()->setDeleted( false );
-		wfLicense::current()->getKeyType();
-	}
+    if ( ! class_exists( 'wfConfig' ) ) {
+        $init();
+    }
+    if ( class_exists( 'wfLicense' ) ) {
+        wfLicense::current()->setType( wfLicense::TYPE_RESPONSE );
+        wfLicense::current()->setPaid( true );
+        wfLicense::current()->setRemainingDays( $RemainingDays );
+        wfLicense::current()->setConflicting( false );
+        wfLicense::current()->setDeleted( false );
+        wfLicense::current()->getKeyType();
+    }
 } );
